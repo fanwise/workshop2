7e1594ad7989a59a802427f9cf3d172a8e4a1397
Wei Fan <weifan@Fanweis-MacBook-Pro.local>
2017-01-16 10:34:38 +0800

    add User model

---
+++ workshop2/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  workshop2
+//
+//  Created by Wei Fan on 1/16/17.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let message: String
+    
+    init?(data:Data) {
+        guard let obj = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:Any] else {
+            return nil
+        }
+        guard let name = obj? ["name"] as? String else {
+            return nil
+        }
+        guard let message = obj? ["message"] as? String else {
+            return nil
+        }
+        
+        self.name = name
+        self.message = message
+        
+    }
+}
