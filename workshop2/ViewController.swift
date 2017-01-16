31ca0bd85b9ba37c0678b39960fc7d7fe2e50cc8
Wei Fan <weifan@Fanweis-MacBook-Pro.local>
2017-01-16 09:49:47 +0800

    create project

---
+++ workshop2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  workshop2
+//
+//  Created by Wei Fan on 1/16/17.
+//  Copyright © 2017 Wei Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
